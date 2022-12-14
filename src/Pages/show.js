6fd7a1ad0390b211c090ebbd1d2c6ff137daddf3
tahmid <nur.tahmid2022@gmail.com>
2022-12-14 14:28:54 +0000

    implemented show route and component

--- src/Pages/show.js
+++ src/Pages/show.js
@@ -2,25 +2,41 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 function Show() {
-  var [name, setName] = useState();
+  var [state, setState] = useState("");
+  var [playerData, setPlayerData] = useState({
+    Matches: "",
+    Runs: "",
+    HS: ""
+  });
 
-  const nameUpdate = (event) => {
-    setName(event.target.value);
+  const handleUpdateSearch = (event) => {
+    setState(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSearch = (event) => {
     event.preventDefault();
+    const data = {
+      Player_Name: state
+    }
 
-    axios.get("https://localhost:3000/show").then((res) => {
-      console.log(res);
-      // window.location = "/retrieve"
-    });
+    axios.get("http://localhost:5000/getData/" + state, data).then(res => {
+      console.log(res.data)
+      setPlayerData(res.data)
+    })
   };
   return (
     <div>
-      <label>Search player: </label>
-      <input type="text"></input>
-      <button type="submit">find</button>
+      <form onSubmit={handleSearch} method="Get">
+        <label>Update player: </label>
+        <input type="text" name="Player_Name" value={state} required onChange={handleUpdateSearch}></input>
+        <button type="submit">find</button>
+      </form>
+      <br />
+
+      {playerData.Matches !== "" ? <p>Matches: {playerData.Matches} </p> : ""}
+      {playerData.Runs !== "" ? <p>Runs: {playerData.Runs} </p> : ""}
+      {playerData.HS !== "" ? <p>HS: {playerData.HS} </p> : ""}
+
     </div>
   );
 }
